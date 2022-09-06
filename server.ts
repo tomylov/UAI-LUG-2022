559b6cbdde2f62c005b229fab39ea1c818105bec
Mauro Falcone <maurofalcone.95@gmail.com>
2022-09-06 15:45:51 -0300

    Setup nodemon, express and typescript

---
+++ server.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Express } from "express";
+import mongoose from "mongoose";
+
+const app: Express = express();
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is up and running at port ${process.env.PORT}`);
+});
+
+connectToDb()
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log(err));
+
+async function connectToDb() {
+  if (process.env.DB_CONNECTION_STRING) {
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+  } else {
+    console.log("Connection string is missing");
+  }
+}
